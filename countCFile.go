8ca515e5b3933fefbaf417bc551ca0750df26da3
DonkeyBurger <donkey_burger@163.com>
2015-04-20 01:42:07 +0800

    init add.

---
+++ countCFile.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type countCFile struct {
+	startRe, endRe *regexp.Regexp
+}
+
+func NewCountCFile() *countCFile {
+	this := &countCFile{}
+	this.startRe = regexp.MustCompile("/\\*")
+	this.endRe = regexp.MustCompile("\\*/")
+	return this
+}
+
+//
+// return value:
+//  allComment:  true: it's an all comment line
+//              false: it's include real code
+//  transition: -1: start comment
+//               0: no change
+//               1: end comment
+func (this *countCFile) checkLine(line string) (allComment bool, transition int) {
+	allComment = true
+	transition = 0
+	matched := false
+
+	matched, _ = regexp.MatchString("^\\s*$", line)
+	if matched {
+		return
+	}
+
+	matched, _ = regexp.MatchString("^\\s*//.*$", line)
+	if matched {
+		return
+	}
+
+	matched, _ = regexp.MatchString("^\\s*\\w\\w*", line)
+	if matched {
+		allComment = false
+	}
+
+	commentStarts := this.startRe.FindStringIndex(line)
+
+	commentEnds := this.endRe.FindStringIndex(line)
+
+	if len(commentStarts) > len(commentEnds) {
+		transition = -1
+	} else if len(commentStarts) < len(commentEnds) {
+		transition = 1
+	}
+	return
+}
+
+func (this *countCFile) countFile(fileName *string) (line int) {
+	line = 0
+	inComment := false
+
+	if *verbose > 3 {
+		fmt.Printf("count: %s\n", fileName)
+	}
+	if !strings.HasSuffix(*fileName, *extension) {
+		return
+	}
+
+	file, err := os.Open(*fileName)
+	if err != nil {
+		log.Fatal(err)
+		return 0
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		allComment, transition := this.checkLine(scanner.Text())
+		if *verbose > 4 {
+			fmt.Printf("%v,%v: %s\n", allComment, transition, scanner.Text())
+		}
+		if inComment {
+			if transition == 0 {
+				continue
+			} else if transition == 1 {
+				inComment = false
+			}
+		} else {
+			if transition == -1 {
+				inComment = true
+			}
+		}
+		if allComment != true {
+			line++
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	err = scanner.Err()
+	if err != nil {
+		log.Println(fileName, ": ", err)
+	}
+	if *verbose > 1 {
+		fmt.Printf("%s:%d\n", fileName, line)
+	}
+	return line
+}
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path"
+)
+
+var verbose *int
+var extension *string
+var cFileCounter *countCFile
+
+func main() {
+	extension = flag.String("ext", "go", "-ext <extension>")
+	verbose = flag.Int("verbose", 0, "-verbose <verbose level>")
+	flag.Parse()
+	sourceRoot := flag.Args()
+
+	if len(sourceRoot) == 0 {
+		sourceRoot = append(sourceRoot, ".")
+	}
+	cFileCounter = NewCountCFile()
+	for _, val := range sourceRoot {
+		line := 0
+
+		fi, err := os.Stat(val)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		if fi.IsDir() {
+			line = countDir(&val)
+		} else {
+			line = getFileCounter().countFile(&val)
+		}
+		fmt.Printf("%s: %d\n", val, line)
+	}
+}
+
+func getFileCounter() (ci CountAFile) {
+	return cFileCounter
+}
+
+type CountAFile interface {
+	countFile(fileName *string) (line int)
+}
+
+func countDir(rootPath *string) (line int) {
+	file, err := os.Open(*rootPath)
+	if err != nil {
+		log.Fatal(err)
+		return 0
+	}
+	defer file.Close()
+
+	fis, err := file.Readdir(0)
+	if err != nil {
+		log.Fatal(err)
+		return 0
+	}
+
+	for _, fi := range fis {
+		filePath := path.Join(*rootPath, fi.Name())
+		if fi.IsDir() {
+			line += countDir(&filePath)
+		} else {
+			line += getFileCounter().countFile(&filePath)
+		}
+	}
+	if *verbose > 0 {
+		fmt.Printf("%s: %d\n", rootPath, line)
+	}
+	return line
+}
